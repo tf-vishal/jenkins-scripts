01ef306baee7fe6714237fd8f6a57f5bbe899c31
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-12 01:10:36 +0530

    testing rollback

--- vars/rollback.groovy
+++ vars/rollback.groovy
@@ -2,7 +2,9 @@ def call(String imageName){
     def prevTag = (BUILD_TAG.toInteger()-1).toString()
     
     sh "docker compose down"
-    sh "sleep 7"
+    sh "fuser -k 8080/tcp || true "
+    sh "fuser -k 3000/tcp || true "
+
     sh "docker pull ${imageName}:${prevTag}"
     sh "sed -i 's/${BUILD_TAG}/${prevTag}/g' docker-compose.yml"
     sh "docker compose up -d"
