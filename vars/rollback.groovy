5b803da92fd9b7196a594d6d27aa78f687361530
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-12 01:01:16 +0530

    testing rollback

--- vars/rollback.groovy
+++ vars/rollback.groovy
@@ -3,7 +3,7 @@ def call(String imageName){
     
     sh "docker compose down"
     sh "sleep 7"x`
-    sh "docker ull ${imageName}:${prevTag}"
+    sh "docker pull ${imageName}:${prevTag}"
     sh "sed -i 's/${BUILD_TAG}/${prevTag}/g' docker-compose.yml"
     sh "docker compose up -d"
 }
