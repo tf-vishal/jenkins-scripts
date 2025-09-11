9903de393347ab41cf4c7f0734b238c89ac59ef3
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-11 17:12:33 +0530

    scripts updated

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -9,10 +9,10 @@ def call(String imageName) {
         echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
 
         echo ' Tagging image: ${imageName}:${BUILD_TAG}'
-        docker tag ${imageName}:${BUILD_TAG} ${DOCKER_USER}/${imageName}:${BUILD_TAG}
+        docker tag ${imageName}:${BUILD_TAG} ${imageName}:${BUILD_TAG}
 
-        echo ' Pushing image to DockerHub: ${DOCKER_USER}/${imageName}:${BUILD_TAG}'
-        docker push ${DOCKER_USER}/${imageName}:${BUILD_TAG}
+        echo ' Pushing image to DockerHub: ${imageName}:${BUILD_TAG}'
+        docker push ${imageName}:${BUILD_TAG}
 
         echo '✅ Docker image pushed successfully!'
         """
