18c78a7d11ff23db7d6460c8de2c99f5c989e5d1
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-11 23:56:17 +0530

    rollback script

---
+++ vars/rollback.groovy
@@ -0,0 +1,8 @@
+def call(String imageName){
+    def prevTag = (BUILD_TAG.toInteger()-1).toString()
+    
+    sh "docker compose down"
+    sh "docker pull ${imageName}:${prevTag}"
+    sh "sed -i "s/${BUILD_TAG}/${prevTag}/g" docker-compose.yml"
+    sh "docker compose up -d"
+}
