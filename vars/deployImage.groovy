b3251c683cabbccde743527992e4c609dfbb1741
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-11 17:21:35 +0530

    scripts updated

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,6 +1,6 @@
 def call(String imageName){
     sh "docker compose down"
-    sh "docker pull ${imageName}:BUILD_TAG"
+    sh "docker pull ${imageName}:${BUILD_TAG}"
     sh "docker compose up -d "
     sh "rm -f ${FRONTEND_PATH}.env ${BACKEND_PATH}.env"
 }
