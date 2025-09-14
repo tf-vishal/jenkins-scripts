c8321babea38443178f302c8e00050c7be1ce122
tf-vishal <vvishalsharma0609@gmail.com>
2025-09-14 15:07:05 +0530

    added port checking

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,5 +1,7 @@
 def call(String imageName){
     sh "docker compose down"
+    sh "fuser -k 8080/tcp || true "
+    sh "fuser -k 3000/tcp || true "
     sh "docker pull ${imageName}:${BUILD_TAG}"
     sh "docker compose up -d "
     sh "rm -f ${FRONTEND_PATH}.env ${BACKEND_PATH}.env"
